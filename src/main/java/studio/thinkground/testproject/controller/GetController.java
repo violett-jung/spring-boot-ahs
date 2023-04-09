6994ea14f03a5c11b6f5ccdefbe74029d76181b5
violett-jung <violett.jung@gmail.com>
2023-04-09 23:02:28 +0900

    스프링 부트를 사용하여 testproject 생성

---
+++ src/main/java/studio/thinkground/testproject/controller/GetController.java
@@ -0,0 +1,10 @@
+package studio.thinkground.testproject.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class GetController {
+
+}
---
+++ src/main/java/studio/thinkground/testproject/controller/HelloController.java
@@ -0,0 +1,16 @@
+package studio.thinkground.testproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+//    @RequestMapping(value="/hello", method = RequestMethod.GET) // 구방식
+    @GetMapping("hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
